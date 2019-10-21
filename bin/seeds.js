5a28235787f171d751434bf2626e1411fe21f893
Daniel Duque <duque@outlook.com>
2019-10-22 01:16:16 +0200

    Iteration 12 done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose  = require('mongoose');
 const Celebrity = require('../models/Celebrity');
+const Movie     = require('../models/Movie');
 
 const dbtitle = "lab-mongoose-movies";
 mongoose.connect(`mongodb://localhost/${dbtitle}`, {
@@ -23,18 +24,60 @@ const celebrities = [
     occupation: 'Actress',
     catchPhrase: 'I volunteer as tribute'
   }
-]
+];
+
+const movies = [
+  {
+    title: 'Bad Boys',
+    genre: 'Action',
+    plot: 'Two hip detectives protect a witness to a murder while investigating a case of stolen heroin from the evidence storage room from their police precinct.'
+  },
+  {
+    title: 'The Hunger Games',
+    genre: 'Adventure',
+    plot: "Katniss Everdeen voluntarily takes her younger sister's place in the Hunger Games: a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death."
+  },
+  {
+    title: 'The Terminator',
+    genre: 'Action',
+    plot: "A seemingly indestructible robot is sent from 2029 to 1984 to assassinate a young waitress, whose unborn son will lead humanity in a war against sentient machines, while a human soldier from the same war is sent to protect her at all costs."
+  }
+];
 
 // Add new celebrities from seed to database
-const createCelebrities = celebrities.map(celebrity => {
-  const newCelebrity = new Celebrity(celebrity)
-  return newCelebrity.save()
-    .then(celebrity => {
-      console.log('Celebrity created correctly: ', celebrity);
-      mongoose.connection.close();
-    })
-    .catch(error => {
-      throw new Error(`Impossible to add the celebrity. ${error}`);
-      mongoose.connection.close();
-    })
-})
+const createCelebrities = (celebrities) => {
+  celebrities.map(celebrity => {
+    const newCelebrity = new Celebrity(celebrity)
+    return newCelebrity.save()
+      .then(celebrity => {
+        console.log('Celebrity created correctly: ', celebrity);
+        mongoose.connection.close();
+      })
+      .catch(error => {
+        throw new Error(`Impossible to add the celebrity. ${error}`);
+        mongoose.connection.close();
+      })
+  })
+};
+
+// Add new movies from seed to database
+const createMovies = (movies) => {
+  movies.map(movie => {
+    const newMovie = new Movie(movie)
+    return newMovie.save()
+      .then(movie => {
+        console.log('Movie created correctly: ', movie);
+        mongoose.connection.close();
+      })
+      .catch(error => {
+        throw new Error(`Impossible to add the movie. ${error}`);
+        mongoose.connection.close();
+      })
+  })
+};
+
+// Call createCelebrities function
+//createCelebrities(celebrities);
+
+// Call createMovies function
+createMovies(movies);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express   = require('express');
 const router    = express.Router();
 const Celebrity = require('../models/Celebrity');
+const Movie     = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* BEGIN CELEBRITIES */
 
 // Celebrities page
 router.get('/celebrities/index', (req, res, next) => {
@@ -80,7 +83,84 @@ router.post('/celebrities/:id', (req, res, next) => {
     })
 });
 
+/* END CELEBRITIES */
 
+/* BEGIN MOVIES */
+
+// Movies page
+router.get('/movies/index', (req, res, next) => {
+  Movie.find()
+    .then(data => {res.render('movies', {data})})
+    .catch(error => {console.log('Error while getting the movies from the DB: ', error)})
+});
+
+// New Movie GET
+router.get('/movies/new', (req, res, next) => {
+  res.render('movies/new');
+});
+
+// New Movie POST
+router.post('/movies', (req, res, next) => {
+  const {title, genre, plot} = req.body;
+  const newMovie = new Movie({title, genre, plot});
+
+  newMovie.save()
+    .then(data => {
+      res.redirect('/movies/index');
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/movies/new');
+    })
+});
+
+// Delete Movie POST
+router.post('/movies/:id/delete', (req, res, next) => {
+  const {id} = req.params;
+  Movie.findByIdAndRemove(id)
+  .then(data => {
+    res.redirect('/movies/index')
+  })
+  .catch(error => {
+    console.log(error);
+    res.redirect('/movies/index')
+  })
+});
+
+// Movie Details page
+router.get('/movies/:id', (req, res, next) =>{
+  const {id} = req.params;
+  Movie.findById(id)
+    .then(data => {res.render('movies/show', {data})})
+    .catch(error => {console.log('Error finding Movie', error)})
+});
+
+// Edit Movie GET
+router.get('/movies/:id/edit', (req, res, next) =>{
+  const {id} = req.params;
+  Movie.findById(id)
+    .then(data => {res.render('movies/edit', {data})})
+    .catch(error => {console.log('Error finding Movie', error)})
+});
+
+// Edit Movie POST
+router.post('/movies/:id', (req, res, next) => {
+  const {id} = req.params;
+  const {title, genre, plot} = req.body;
+  Movie.findOneAndUpdate(
+    {_id: id},
+    {$set: {title, genre, plot}},
+    {new: true})
+    .then(data => {
+      res.redirect('/movies/index');
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/movies/' + id + '/edit');
+    })
+});
+
+/* END MOVIES */
 
 
 module.exports = router;
