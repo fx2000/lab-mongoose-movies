36717af87e54b5af52334abfcf16f36982ba0049
Daniel Duque <duque@outlook.com>
2019-10-22 00:46:32 +0200

    Finished Iteration 6

--- routes/index.js
+++ routes/index.js
@@ -32,11 +32,11 @@ router.post('/celebrities', (req, res, next) => {
       console.log(error);
       res.redirect('/celebrities/new');
     })
-})
+});
 
 // Delete Celebrity POST
 router.post('/celebrities/:id/delete', (req, res, next) => {
-  let {id} = req.params;
+  const {id} = req.params;
   Celebrity.findByIdAndRemove(id)
   .then(data => {
     res.redirect('/celebrities/index')
@@ -45,14 +45,42 @@ router.post('/celebrities/:id/delete', (req, res, next) => {
     console.log(error);
     res.redirect('/celebrities/index')
   })
-})
+});
 
 // Celebrity Details page
 router.get('/celebrities/:id', (req, res, next) =>{
-  let {id} = req.params;
+  const {id} = req.params;
   Celebrity.findById(id)
     .then(data => {res.render('celebrities/show', {data})})
     .catch(error => {console.log('Error finding celebrity', error)})
-})
+});
+
+// Edit Celebrity GET
+router.get('/celebrities/:id/edit', (req, res, next) =>{
+  const {id} = req.params;
+  Celebrity.findById(id)
+    .then(data => {res.render('celebrities/edit', {data})})
+    .catch(error => {console.log('Error finding celebrity', error)})
+});
+
+// Edit Celebrity POST
+router.post('/celebrities/:id', (req, res, next) => {
+  const {id} = req.params;
+  const {name, occupation, catchPhrase} = req.body;
+  Celebrity.findOneAndUpdate(
+    {_id: id},
+    {$set: {name, occupation, catchPhrase}},
+    {new: true})
+    .then(data => {
+      res.redirect('/celebrities/index');
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/celebrities/' + id + '/edit');
+    })
+});
+
+
+
 
 module.exports = router;
